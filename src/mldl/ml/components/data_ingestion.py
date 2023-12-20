b9bd0e60b66d5d9f7621e7ee6cc7eb217eeb5e69
juliusolaifa <jbolaifa@gmail.com>
2023-12-20 17:01:05 -0600

    model trainer

--- src/mldl/ml/components/data_ingestion.py
+++ src/mldl/ml/components/data_ingestion.py
@@ -8,6 +8,7 @@ from util.exception import CustomException
 from util.logger import logging
 
 from mldl.ml.components.data_transformation import DataTransformation
+from mldl.ml.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -51,4 +52,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
