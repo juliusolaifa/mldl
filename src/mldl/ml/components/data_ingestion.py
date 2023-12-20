c5711933f0ea22bc095b10d2f9b6c20d9e64f4dc
juliusolaifa <jbolaifa@gmail.com>
2023-12-20 11:59:27 -0600

    Data Transformation done

--- src/mldl/ml/components/data_ingestion.py
+++ src/mldl/ml/components/data_ingestion.py
@@ -7,6 +7,9 @@ from dataclasses import dataclass
 from util.exception import CustomException
 from util.logger import logging
 
+from mldl.ml.components.data_transformation import DataTransformation
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -45,4 +48,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
